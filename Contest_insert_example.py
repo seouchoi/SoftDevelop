cbf69128bb5875f005b2deeb27f088a420f828c2
박상하 <sangha326@naver.com>
2024-12-01 01:57:34 +0900

    12/1 1:57 박상하

--- Contest_insert_example.py
+++ Contest_insert_example.py
@@ -62,81 +62,204 @@ contests = [
         "image_path": "poster/poster5.png"
     },
     {
-        "contest_name": "친환경 에너지 관리 시스템 공모전",
-        "max_participate": 3,
-        "application_start": datetime(2024, 11, 4),
-        "application_end": datetime(2025, 1, 9),
-        "contest_start": datetime(2025, 1, 13),
-        "contest_end": datetime(2025, 1, 16),
-        "category_id": 8,  # 건축
-        "organization_website": "https://via.placeholder.com/300x200",
-        "image_path": "poster/poster5.png"
-    },
-
-    {
-        title: "2024년 NIA 지속 가능한 에너지 솔루션 제안 공모전",
-        description: "친환경 에너지 및 지속가능한 에너지 솔루션 아이디어를 모집합니다.",
-        deadline: "2024-11-30",
-        organizer: "한국전력공사(KEPCO)",
-        image: "poster/contest1.png",
-        url: "https://www.nia.or.kr/site/nia_kor/ex/bbs/View.do?cbIdx=99835&bcIdx=27191&parentSeq=27191"
+        "contest_name": "2024년 NIA 지속 가능한 에너지 솔루션 제안 공모전",
+        "max_participate": 1,
+        "application_start": datetime(2024, 9, 1),
+        "application_end": datetime(2025, 9, 30),
+        "contest_start": datetime(2025, 11, 1),
+        "contest_end": datetime(2025, 11, 8),
+        "category_id": 7,  # 에너지
+        "organization_website": "https://www.nia.or.kr/site/nia_kor/ex/bbs/View.do?cbIdx=99835&bcIdx=27191&parentSeq=27191",
+        "image_path": "poster/contest1.png"
     },
     {
-        title: "2024년 서울 지능형 사물인터넷(AIoT) 해커톤 대회",
-        description: "인공지능(AI) 기술을 접목한 IoT 기기 및 시스템을 설계하여 지능형 사물인터넷 솔루션을 개발하세요.",
-        deadline: "2024-08-31",
-        organizer: "서울특별시",
-        image: "poster/contest2.png",
-        url: "https://www.seoul.go.kr/news/news_notice.do#view/415895"
+        "contest_name": "2024년 서울 지능형 사물인터넷(AIoT) 해커톤 대회",
+        "max_participate": 4,
+        "application_start": datetime(2024,7, 19),
+        "application_end": datetime(2024, 8, 31),
+        "contest_start": datetime(2024, 10, 10),
+        "contest_end": datetime(2024, 10, 11),
+        "category_id": 10,  # 로봇공학 자동화
+        "organization_website": "https://www.seoul.go.kr/news/news_notice.do#view/415895",
+        "image_path": "poster/contest2.png"
     },
     {
-        title: "2024년 AI·미디어 융합기술 공모전",
-        description: "AI 기술을 활용한 혁신적인 아이디어 공모전입니다.",
-        deadline: "2024-09-23",
-        organizer: "과학기술정보통신부",
-        image: "poster/contest3.png",
-        url: "https://www.msit.go.kr/bbs/view.do?sCode=user&mId=129&mPid=224&pageIndex=&bbsSeqNo=100&nttSeqNo=3179039&searchOpt=ALL&searchTxt="
+        "contest_name": "2024년 AI·미디어 융합기술 공모전",
+        "max_participate": 1,
+        "application_start": datetime(2024, 7, 25),
+        "application_end": datetime(2024, 9, 23),
+        "contest_start": datetime(2024, 7, 25),
+        "contest_end": datetime(2024, 9, 2.23),
+        "category_id": 10,  # 로봇공학 자동화
+        "organization_website": "https://www.msit.go.kr/bbs/view.do?sCode=user&mId=129&mPid=224&pageIndex=&bbsSeqNo=100&nttSeqNo=3179039&searchOpt=ALL&searchTxt=",
+        "image_path": "poster/contest3.png"
     },
     {
-        title: "2024년 항공우주논문상 공모전",
-        description: "항공우주 산업과 4차 산업혁명 기술의 융합을 연구하며, 창의적인 아이디어를 제시하세요.",
-        deadline: "2024-09-30",
-        organizer: "한국항공우주산업(KAI)",
-        image: "poster/contest4.png",
-        url: "https://m.koreaaero.com/KO/Sustainability/AerospaceJournalPrizeInfo.aspx"
+        "contest_name": "K-디지털 트레이닝 해커톤 2024",
+        "max_participate": 6,
+        "application_start": datetime(2024, 8, 19),
+        "application_end": datetime(2024, 9, 6),
+        "contest_start": datetime(2024, 8, 19),
+        "contest_end": datetime(2024, 9, 6),
+        "category_id": 10,  #로봇공학 자동화 
+        "organization_website": "http://k-digitalhackathon.kr/",
+        "image_path": "poster/contest5.png"
     },
     {
-        title: "디지털 혁신 해커톤 2024",
-        description: "참가자들은 48시간 동안 팀을 이루어 문제 해결 프로토타입을 제작하고 발표합니다.",
-        deadline: "2024-09-06",
-        organizer: "고용노동부, 한국기술교육대학교 직업능력심사평가원",
-        image: "poster/contest5.png",
-        url: "http://k-digitalhackathon.kr/"
+        "contest_name": "석유 화학 올림피아드",
+        "max_participate": 1,
+        "application_start": datetime(2024, 7, 1),
+        "application_end": datetime(2024, 7, 6),
+        "contest_start": datetime(2024, 7, 12),
+        "contest_end": datetime(2024, 8, 12),
+        "category_id": 11,  # 화학공학
+        "organization_website": "https://www.lgchem.com/main/index",
+        "image_path": "poster/contest6.png"
     },
     {
-        title: "석유 화학 올림피아드",
-        description: "탄소중립 시대를 위한 친환경 화학 공정과 새로운 재활용 기술을 개발하는 아이디어를 찾는 공모전입니다.",
-        deadline: "2024-11-11",
-        organizer: "LG화학, 한국화학공학회",
-        image: "poster/contest6.png",
-        url: "https://www.lgchem.com/main/index"
+        "contest_name": "나노영챌린지 2024 대학(원)생 나노기술 공모전",
+        "max_participate": 1,
+        "application_start": datetime(2024, 3, 21),
+        "application_end": datetime(2024, 4, 10),
+        "contest_start": datetime(2024, 6, 1),
+        "contest_end": datetime(2024, 9, 23),
+        "category_id": 11,  # 화학공학
+        "organization_website": "https://www.kontrs.or.kr/main/main.php",
+        "image_path": "poster/contest8.png"
     },
     {
-        title: "88로봇데이 차세대로봇 아이디어 공모전",
-        description: "고령자를 돕거나 지구 환경에 유익한 로봇 아이디어를 제시하는 공모전입니다.",
-        deadline: "2024-12-30",
-        organizer: "한양대학교 ERICA 지능형로봇사업단",
-        image: "poster/contest7.png",
-        url: "https://www.etedu.co.kr/shop/item.php?it_id=1657070988"
+        "contest_name":"Blaybus 실전 앱 개발 경진대회",
+        "max_participate":7,
+        "application_start":datetime(2024,12,2),
+        "application_end":datetime(2024,12,29),
+        "contest_start":datetime(2025,1,5),
+        "contest_end":datetime(2025,1,17),
+        "category_id":12,
+        "organization_website":"https://www.blaybus.com/activities/407/landing",
+        "image_path":"poster/contest9.png"
     },
     {
-        title: "나노영챌린지 2024 대학(원)생 나노기술 공모전",
-        description: "나노기술을 활용한 첨단 신소재 개발과 응용 사례를 제시하는 공모전입니다.",
-        deadline: "2024-04-10",
-        organizer: "나노기술연구협회",
-        image: "poster/contest8.png",
-        url: "https://www.kontrs.or.kr/main/main.php"
+        "contest_name":"2025 전국 대학생 대상 자작미래자동차 경진대회",
+        "max_participate":4,
+        "application_start":datetime(2024,11,22),
+        "application_end":datetime(2024,12,6),
+        "contest_start":datetime(2025,1,16),
+        "contest_end":datetime(2025,1,17),
+        "category_id":4,
+        "organization_website":"https://www.wevity.com/index_university.php?c=find&s=_university&gbn=viewok&gp=4&ix=93575",
+        "image_path":"poster/contest10.jpg"
+    },
+    {
+        "contest_name":"원자력 미래기술 아이디어 부트캠프",
+        "max_participate":0,
+        "application_start":datetime(2024,11,20),
+        "application_end":datetime(2024,12,17),
+        "contest_start":datetime(2025,1,13),
+        "contest_end":datetime(2025,1,24),
+        "category_id":11,
+        "organization_website":"https://www.konicof.or.kr/neti/html/sub05/0501.html?mode=V&no=2c5658c4e5b49ee04fb7ee30328ebf46",
+        "image_path":"poster/contest11.jpg"
+    },
+    {
+        "contest_name":"[알서포트] AI x 원격 솔루션 아이디어 모집",
+        "max_participate":1,
+        "application_start":datetime(2024,11,18),
+        "application_end":datetime(2024,12,17),
+        "contest_start":datetime(2024,11,18),
+        "contest_end":datetime(2024,12,17),
+        "category_id":10,
+        "organization_website":"https://content.rview.com/ko/blog/promotion-airchallenge/?utm_source=linkareer&utm_medium=referral&utm_campaign=rsko_2411_prm_airchallenge&utm_content=cta1_brandsite",
+        "image_path":"poster/contest12.png"
+    },
+    {
+        "contest_name":"화성시 동부권 공공정원화 설계공모",
+        "max_participate":1,
+        "application_start":datetime(2024,11,6),
+        "application_end":datetime(2024,12,5),
+        "contest_start":datetime(2025,1,14),
+        "contest_end":datetime(2025,1,14),
+        "category_id":8,
+        "organization_website":"https://www.c3ka.com/design-contest-for-public-garden-in-the-eastern-district-of-hwaseong-city/",
+        "image_path":"poster/contest13.png"
+    },
+    {
+        "contest_name":"제 4회 WONDERCHILD 창의발명대회",
+        "max_participate":2,
+        "application_start":datetime(2024,11,15),
+        "application_end":datetime(2025,1,20),
+        "contest_start":datetime(2025,2,7),
+        "contest_end":datetime(2025,2,8),
+        "category_id":5,
+        "organization_website":"https://wonderchild.co.kr/",
+        "image_path":""
+    },
+    {
+        "contest_name":"[데이콘] 이미지 색상화 및 손실 부분 복원 AI 경진대회 : 월간 데이콘(~12/9)",
+        "max_participate":1,
+        "application_start":datetime(2024,11,12),
+        "application_end":datetime(2024,12,9),
+        "contest_start":datetime(2024,11,12),
+        "contest_end":datetime(2024,12,9),
+        "category_id":10,
+        "organization_website":"https://dacon.io/competitions/official/236420/overview/description",
+        "image_path":""
+    },
+    {
+        "contest_name":"글로벌 청년 임팩트 해커톤",
+        "max_participate":1,
+        "application_start":datetime(2024,11,18),
+        "application_end":datetime(2024,12,8),
+        "contest_start":datetime(2024,12,27),
+        "contest_end":datetime(2024,12,28),
+        "category_id":12,
+        "organization_website":"https://wfuna.or.kr/program_global_impact_hackathon_2024",
+        "image_path":"poster/contest14.png"
+    },
+    {
+        "contest_name":"제 2회 (주) NovaLoop배 게임 제작 공모전",
+        "max_participate":4,
+        "application_start":datetime(2024,11,1),
+        "application_end":datetime(2024,11,30),
+        "contest_start":datetime(2024,11,1),
+        "contest_end":datetime(2024,11,30),
+        "category_id":12,
+        "organization_website":"https://docs.google.com/forms/d/e/1FAIpQLSc_NE3tbZXwgwAtJ2vETeIi1kvGqDeysLXeoA__NAXHFuFYIg/viewform",
+        "image_path":""
+    },
+    {
+        "contest_name":"헬로소프트 코스페이시스 작품 공모전",
+        "max_participate":1,
+        "application_start":datetime(2024,11,17),
+        "application_end":datetime(2024,11,30),
+        "contest_start":datetime(2024,11,17),
+        "contest_end":datetime(2024,11,30),
+        "category_id":12,
+        "organization_website":"https://cafe.naver.com/cospaces/",
+        "image_path":"poster/contest15.png"
+    },
+    {
+        "contest_name":"컴투스 글로벌 게임개발 공모전 ‘컴:온 2024’",
+        "max_participate":10,
+        "application_start":datetime(2024,11,19),
+        "application_end":datetime(2024,12,30),
+        "contest_start":datetime(2024,11,19),
+        "contest_end":datetime(2024,12,30),
+        "category_id":12,
+        "organization_website":"https://www.com2us.com/gamecontest",
+        "image_path":""
+    },
+    {
+        "contest_name":"2024~2025년 한국석유공사 데이터 분석 공모전",
+        "max_participate":4,
+        "application_start":datetime(2024,10,14),
+        "application_end":datetime(2025,1,22),
+        "contest_start":datetime(2024,10,14),
+        "contest_end":datetime(2025,1,22),
+        "category_id":12,
+        "organization_website":"https://www.knoc.co.kr/sub05/sub05_5_5.jsp?page=1&num=243&mode=view&field=&text=&bid=DATA5&ses=USERSESSION&psize=12",
+        "image_path":""
     }
+
     ]
 
 
