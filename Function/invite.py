5a4800e14f9b7e3b3bd606a01080058dc8edb0bf
Choi seo woo <tjdn1080@gmail.com>
2024-12-04 17:20:09 +0900

    12/04 17:19 최서우

--- Function/invite.py
+++ Function/invite.py
@@ -8,6 +8,5 @@ invite_bp = Blueprint('invite', __name__)
 @invite_bp.route('/api/invite', methods=['POST'])
 def invite():
     data = request.get_json()
-    
     invite_db_handler.insert_invite(data['sender_id'], int(data['team_id']) , data['receiver_id'], data['message'], data['contest_id'])
     return jsonify(data), 200
--- Function/invite_detail.py
+++ Function/invite_detail.py
@@ -14,10 +14,11 @@ def show_detail(invite_id):
     contest_id = invite_data[0]['contest_id']
     sender_id = invite_data[0]['sender_id']
     team_id = invite_data[0]['team_id']
+    receiver_id = invite_data[0]['receiver_id']
     
     contest_data = contest_db_handler.get_contest_by_id(contest_id)
     sender_data = member_db_handler.get_member_data_for_key(sender_id)
     invite_db_handler.mark_invite_as_read(invite_id)
     
     return render_template('invite_detail_page.html', contest_data = contest_data, member_data = sender_data
-                           ,team_id = team_id), 200
+                           ,team_id = team_id, receiver_id = receiver_id), 200
